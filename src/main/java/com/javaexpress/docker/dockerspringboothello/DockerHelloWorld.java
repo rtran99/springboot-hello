410fe03087229a7daa95f0b00ebe58a7129ec246
rtran99 <62718771+rtran99@users.noreply.github.com>
2023-03-09 15:48:33 -0800

    Update DockerHelloWorld.java

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
@@ -11,8 +11,8 @@ public class DockerHelloWorld {
 	@GetMapping
 	public String getName() {
 		
-		return "Deployed by Rony"
-		return "Welcome to deployment in docker";
 		
+		return "Welcome to Rony's deployment in docker";
+
 	}
 }
